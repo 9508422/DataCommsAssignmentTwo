76cfb38a49d51edcc31b8c6271caf14c1cf52d8f
Rhys Gevaux <9508422@student.swin.edu.au>
2015-05-21 22:42:30 +1000

    args fix

--- HelloServer/src/com/rhys/helloserver/UDPServer.java
+++ HelloServer/src/com/rhys/helloserver/UDPServer.java
@@ -9,16 +9,21 @@ import java.net.DatagramSocket;
 class UDPServer {
 
     public static void main(String[] args) throws Exception {
-        DatagramSocket serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
-        byte[] receiveData = new byte[1024];
-        byte[] sendData;
-        while (true) {
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            serverSocket.receive(receivePacket);
-            String sentence = new String(receivePacket.getData());
-            System.out.println("Received: " + sentence);
-            sendData = "Hello, my name is Rhys Gevaux and my ID is 9508422".getBytes();
-            serverSocket.send(new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort()));
+        if (args.length < 1) {
+            System.out.println("No arguments entered!");
+            System.exit(1);
+        } else {
+            DatagramSocket serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
+            byte[] receiveData = new byte[1024];
+            byte[] sendData;
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                String sentence = new String(receivePacket.getData());
+                System.out.println("Received: " + sentence);
+                sendData = "Hello, my name is Rhys Gevaux and my ID is 9508422".getBytes();
+                serverSocket.send(new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort()));
+            }
         }
     }
 }
