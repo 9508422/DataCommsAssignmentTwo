401715cafe52adaef1adcdbf7162cfe41602eb1d
Rhys Gevaux <9508422@student.swin.edu.au>
2015-05-22 01:43:10 +1000

    HelloServer: complete

--- HelloServer/src/com/rhys/helloserver/UDPClient.java
+++ HelloServer/src/com/rhys/helloserver/UDPClient.java
@@ -7,27 +7,47 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 /**
- * Created by Rhys on 21/05/2015.
+ * Created by Rhys Gevaux on 21/05/2015.
+ * @author Rhys Gevaux 9508422
+ *
+ * Reference: https://systembash.com/a-simple-java-udp-server-and-udp-client/
  */
 class UDPClient {
 
+    /**
+     * Connects to a UDP server defined by user input, user is prompted to send the server a message.
+     * @param args - unused
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
+        // BufferedReader initiated to read from console input
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Server IP: ");
+        // user inputs server IP
+        System.out.println("Server IP:");
         String serverIP = inFromUser.readLine();
 
-        System.out.println("Server port: ");
+        // user inputs server port
+        System.out.println("Server port:");
         String serverPort = inFromUser.readLine();
 
-        DatagramSocket clientSocket = new DatagramSocket();
-        byte[] sendData;
-        byte[] receiveData = new byte[1024];
+        DatagramSocket clientSocket = new DatagramSocket(); // new socket for request
+        byte[] sendData; // byte array to store the UDP request packet sent to the server
+        byte[] receiveData = new byte[1024]; // byte array to store the UDP response packet received from the server
+
+        // user inputs desired message to send to the server
+        System.out.println("Send a message to the server:");
         sendData = inFromUser.readLine().getBytes();
-        clientSocket.send(new DatagramPacket(sendData, sendData.length, InetAddress.getByName(serverIP), Integer.parseInt(serverPort)));
+
+        // UDP request packet is created and sent through the previously defined socket to the server
+        clientSocket.send(new DatagramPacket(sendData, sendData.length, InetAddress.getByName(serverIP),
+                Integer.parseInt(serverPort)));
+
+        // new packet to receive the UDP response from the server
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        clientSocket.receive(receivePacket);
-        System.out.println("From server: " + new String(receivePacket.getData()));
-        clientSocket.close();
+
+        clientSocket.receive(receivePacket); // UDP response packet received
+        System.out.println("From server: " + new String(receivePacket.getData())); // output response to console
+        clientSocket.close(); // disconnect from the server
     }
 }
--- HelloServer/src/com/rhys/helloserver/UDPServer.java
+++ HelloServer/src/com/rhys/helloserver/UDPServer.java
@@ -4,25 +4,43 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 /**
- * Created by Rhys on 21/05/2015.
+ * Created by Rhys Gevaux on 21/05/2015.
+ * @author Rhys Gevaux 9508422
+ *
+ * Reference: https://systembash.com/a-simple-java-udp-server-and-udp-client/
  */
 class UDPServer {
 
+    /**
+     * Starts up a UDP server that response with my name and student ID when prompted.
+     * @param args allows port input
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
+        // checks for arguments
         if (args.length < 1) {
-            System.out.println("No arguments entered!");
+            // reports an error if no arguments are entered and exits
+            System.err.println("No arguments entered!");
             System.exit(1);
         } else {
+            // new socket to receive request through defined port
             DatagramSocket serverSocket = new DatagramSocket(Integer.parseInt(args[0]));
-            byte[] receiveData = new byte[1024];
-            byte[] sendData;
+
+            byte[] receiveData = new byte[1024]; // byte array to store the UDP request packet received from the client
+            byte[] sendData; // byte array to store the UDP response packet sent to the client
+
+            // endless loop to keep server running
             while (true) {
+                // new packet to receive UDP request from the client
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                serverSocket.receive(receivePacket);
-                String sentence = new String(receivePacket.getData());
-                System.out.println("Received: " + sentence);
-                sendData = "Hello, my name is Rhys Gevaux and my ID is 9508422".getBytes();
-                serverSocket.send(new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort()));
+
+                serverSocket.receive(receivePacket); // receive the packet
+                System.out.println("Received: " + receivePacket.getData()); // output the request to console
+                sendData = "Hello, my name is Rhys Gevaux and my ID is 9508422".getBytes(); // define the response
+
+                // send the UDP response to the client
+                serverSocket.send(new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(),
+                        receivePacket.getPort()));
             }
         }
     }
